37450f933162f4afcb38653dba412a64d87ee419
Country-If <2450854008@qq.com>
2021-11-30 12:36:13 +0800

    register.py

--- register.py
+++ register.py
@@ -3,11 +3,7 @@
 
 __author__ = "Maylon"
 
-
-from PySide2.QtWidgets import QApplication
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile
-import sys
+from tool import *
 
 
 class register_ui:
@@ -17,6 +13,23 @@ class register_ui:
         qfile.close()
 
         self.ui = QUiLoader().load(qfile)
+
+        self.ui.confirm_label.returnPressed.connect(self.register)
+        self.ui.register_btn.clicked.connect(self.register)
+
+    def register(self):
+        username = self.ui.username_label.text()
+        password = self.ui.password_label.text()
+        confirm_passwd = self.ui.confirm_label.text()
+
+        if username == '':
+            QMessageBox.critical(self.ui, '错误', '用户名不能为空')
+        elif password == '':
+            QMessageBox.critical(self.ui, '错误', '密码不能为空')
+        elif confirm_passwd == '':
+            QMessageBox.critical(self.ui, '错误', '确认密码不能为空')
+        elif password != confirm_passwd:
+            QMessageBox.critical(self.ui, '错误', '两次输入的密码不同！')
 
 
 if __name__ == '__main__':
