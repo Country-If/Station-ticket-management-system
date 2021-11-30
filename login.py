ab183e9d17e33be5352243ddce462ad8364ae126
Country-If <2450854008@qq.com>
2021-11-30 15:05:59 +0800

    update login.py

--- login.py
+++ login.py
@@ -9,17 +9,10 @@ from tool import *
 class login_ui:
     """登录界面"""
 
-    def __init__(self, connect_obj, cursor):
+    def __init__(self):
         """
         实例化对象
-
-        :param connect_obj: 数据库连接对象
-        :param cursor: 数据库游标对象
         """
-        # 获取数据库连接
-        self.connect_obj = connect_obj
-        self.cursor = cursor
-
         # 动态加载界面
         qfile = QFile("login.ui")
         qfile.open(QFile.ReadOnly)
