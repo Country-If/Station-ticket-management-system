617a47a00490fc980169dc4b459667c0cdf7f650
Country-If <2450854008@qq.com>
2021-11-30 13:57:20 +0800

    add comment

--- register.py
+++ register.py
@@ -7,23 +7,41 @@ from tool import *
 
 
 class register_ui:
+    """注册界面"""
+
     def __init__(self, connect_obj, cursor):
+        """
+        实例化对象
+
+        :param connect_obj: 数据库连接对象
+        :param cursor: 数据库游标对象
+        """
+        # 获取数据库连接
         self.connect_obj = connect_obj
         self.cursor = cursor
+
+        # 动态加载界面
         qfile = QFile("register.ui")
         qfile.open(QFile.ReadOnly)
         qfile.close()
-
         self.ui = QUiLoader().load(qfile)
 
+        # 信号与槽连接
         self.ui.confirm_label.returnPressed.connect(self.register)
         self.ui.register_btn.clicked.connect(self.register)
 
     def register(self):
+        """
+        注册函数
+
+        :return: None
+        """
+        # 获取输入框文本
         username = self.ui.username_label.text()
         password = self.ui.password_label.text()
         confirm_passwd = self.ui.confirm_label.text()
 
+        # 对输入信息进行检查
         if username == '':
             QMessageBox.critical(self.ui, '错误', '用户名不能为空')
         elif password == '':
@@ -33,6 +51,7 @@ class register_ui:
         elif password != confirm_passwd:
             QMessageBox.critical(self.ui, '错误', '两次输入的密码不同！')
 
+        # 执行SQL语句
         sql = r"insert into user_information(user_name, password, target) values('%s', '%s', '%s');" \
               % (username, password, '0')
         try:
@@ -45,6 +64,11 @@ class register_ui:
             self.connect_obj.rollback()
 
     def clear(self):
+        """
+        清空文本框
+
+        :return: None
+        """
         self.ui.username_label.clear()
         self.ui.password_label.clear()
         self.ui.confirm_label.clear()
