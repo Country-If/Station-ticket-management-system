5f98ae573b800c81bcebf30a03d26719c0a01889
Country-If <2450854008@qq.com>
2021-11-30 15:54:39 +0800

    update tool.py

--- tool.py
+++ tool.py
@@ -5,7 +5,7 @@ __author__ = "Maylon"
 
 from PySide2.QtWidgets import QApplication, QMessageBox
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtCore import QFile
+from PySide2.QtCore import QFile, QDate
 import sys
 import pymysql
 from sshtunnel import SSHTunnelForwarder
