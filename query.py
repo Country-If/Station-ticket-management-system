1136053cf32ae791ffccf9b12c2b5f5432027e5e
Country-If <2450854008@qq.com>
2021-11-30 15:54:28 +0800

    complete query function

---
+++ query.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
+from tool import *
+
+
+class query_ui:
+    """查询界面"""
+
+    def __init__(self):
+        """
+        实例化对象
+        """
+        # 动态加载界面
+        qfile = QFile("query.ui")
+        qfile.open(QFile.ReadOnly)
+        qfile.close()
+        self.ui = QUiLoader().load(qfile)
+        self.ui.date_chosen.setDate(QDate.currentDate())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = query_ui()
+    window.ui.show()
+    app.exec_()
